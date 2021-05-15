961cf40ffc9c1366b7c367ea156662cafe793027
Victor Onyebuchi <victorchike247@gmail.com>
2021-05-16 00:18:59 +0100

    made one liner

--- js/main.js
+++ js/main.js
@@ -2,9 +2,7 @@ const editor = document.getElementById('editor');
 const preview = document.getElementById('preview');
 
 const runPreview = () => {
-    marked.setOptions({
-        breaks: true,
-    });
+    marked.setOptions({ breaks: true });
 
     const text = editor.value;
     preview.innerHTML = marked(text);  
